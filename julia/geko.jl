cf81b4dfbd2600455e7db552ee6382f359acb300
adam-says <amoreira@sissa.it>
2023-01-30 11:20:19 +0100

    Begin work on Julia wrapper

---
+++ julia/geko.jl
@@ -0,0 +1,86 @@
+# Julia wrapper for geko
+#
+# Adam Armada-Moreira, Jan 2023
+
+function parseInput(usr_input_raw)
+
+    fs = 15000; # Sampling rate
+    nA = 1; # Number of active channels
+    filename = ""; # Name of the experiment
+    mode = "VC"; # Mode of the experiment
+    stimFile = "default.stim";
+    strForGeko_pre = "./geko ";
+
+    usr_input = replace(usr_input_raw, ' ' => "");
+    usr_input = split(usr_input, '-')
+    usr_input = String.(usr_input)
+
+    #=========================#
+    # Configuration arguments #
+    #=========================#
+
+    if usr_input[1] == "set"
+        print("Setting experiment configuration:\n\n")
+        for i=2:lastindex(usr_input)
+            if usr_input[i][1] == 's'
+                print("Sampling rate: ", usr_input[i][2:end], " Hz", "\n")
+                fs = parse(Int, usr_input[i][2:end]);
+            elseif usr_input[i][1] == 'c'
+                print("Number of active channels: ", usr_input[i][2:end], "\n")
+                nA = parse(Int, usr_input[i][2:end]);
+            elseif usr_input[i][1] == 'm'
+                print("Mode: ", usr_input[i][2:end], "\n")
+                mode = usr_input[i][2:end];
+            elseif usr_input[i][1] == 'f'
+                print("Experiment name: ", usr_input[i][2:end], "\n")
+                filename = usr_input[i][2:end];
+            else
+                print("Invalid option: ", usr_input[i][1], "\n")
+            end
+        end
+        strForGeko_pre = "./geko -s $fs -c $nA -m $mode -f $filename";
+    end
+
+    #=========================#
+    # Run an experiment       #
+    #=========================#
+
+    if usr_input[1] == "run"
+        for i=2:lastindex(usr_input)
+            if usr_input[i][1] == 'o'
+                stimFile = usr_input[i][2:end];
+                print("Loading stimulation file $stimFile\n")
+            end
+        end
+        if length(stimFile) == 0
+            print("No stimulation file was provided. Using default.\n")
+        end
+        print("Running experiment...\n\n")
+        strForGeko = strForGeko_pre * " -o $stimFile";
+        # TODO: SEND THIS COMMAND TO GEKO
+        # GET DATA BACK, PLOT IT
+    end
+
+    #=========================#
+    # Create a new stim file  #
+    #=========================#
+
+    if usr_input[1] == "stim"
+        print("Creating new stimulation file:\n\n")
+    end
+
+end
+
+print("Usage: geko.jl [options]\n")
+print("     set -s -c -m -f\n\n")
+print("     run -o\n\n")
+print("     stim -s -c -m -f\n\n")
+print("     exit\n\n")
+
+while true
+    usr_input_raw = readline();
+    parseInput(usr_input_raw)
+    if usr_input_raw == "exit"
+        break
+    end
+end
